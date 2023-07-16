bcb70e0281d307d70b1cccf5f7e147429b1cee23
KriXPello <krixpello@gmail.com>
2023-07-16 12:44:40 +0300

    nested key support optional fields

--- nested-key.ts
+++ nested-key.ts
@@ -2,11 +2,11 @@
  * Делает Union из ключей объекта (если есть вложенные объекты, объединяет через точку)
  */
 type NestedKey<O extends Record<string, any>> = {
-  [K in Extract<keyof O, string>]: O[K] extends Array<any>
+  [K in Extract<keyof O, string>]: Required<O>[K] extends Array<any>
   ? K // проверка нужна, иначе для поля, значение которого - массив вам будут показаны все методы массива
-  : O[K] extends Record<string, any>
+  : Required<O>[K] extends Record<string, any>
   // @ts-ignore ругается, мол, может быть бесконечно глубоким (неправда)
-  ? `${K}` | `${K}.${NestedKey<O[K]>}`
+  ? `${K}` | `${K}.${NestedKey<Required<O>[K]>}`
   : K
 }[Extract<keyof O, string>];
 
